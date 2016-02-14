63d748292707c7e2e8463e3dd72b909e7db68f3f
nikolai996 <jakimychev.nikolaj@gmail.com>
2016-02-14 23:24:31 +0300

    Update main.cpp

--- Lab1/2.6/main.cpp
+++ Lab1/2.6/main.cpp
@@ -3,8 +3,10 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
+int main(int argc, char *argv[]) 
+{
+    if (argc != 4) 
+    {
         printf("\nNumber parameters is wrong");
         return -1;
     }
@@ -15,20 +17,24 @@ int main(int argc, char *argv[]) {
     numb = *&argv[3];
     double C = atof(numb);
     double Discriminant = B * B - 4 * A * C;
-    if (A == 0) {
+    if (A == 0)
+    {
         printf("\nError, it's a not quadratic equation");
         return -1;
     }
-    if (Discriminant > 0) {
+    if (Discriminant > 0)
+    {
         double x1 = (-B + sqrt(Discriminant)) / (2 * A);
         double x2 = (-B - sqrt(Discriminant)) / (2 * A);
         printf("Roots of equation: %6.4f %6.4f\n", x1, x2);
     }
-    else if (Discriminant == 0) {
+    else if (Discriminant == 0) 
+    {
         double x = -B / (2 * A);
         printf("Root of equation: %6.4f\n", x);
     }
-    else {
+    else 
+    {
         printf("\nThere is no real root");
     }
     return 0;
