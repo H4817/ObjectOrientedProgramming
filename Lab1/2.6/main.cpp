06749e00ce651919bcee46be727013a5422cc56c
nikolai996 <jakimychev.nikolaj@gmail.com>
2016-02-17 13:02:06 +0300

    Update main.cpp

--- Lab1/2.6/main.cpp
+++ Lab1/2.6/main.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]) 
+double CalculatingDiscriminant(double &A, double &B, double &C)
 {
-    if (argc != 4) 
+    return B * B - 4 * A * C;
+}
+
+vector<double> CalculatingRootsOfEquation(double &A, double &B, double &C, double &Discriminant)
+{
+    vector<double> roots;
+    if (A == 0)
+    {
+        return roots;
+    }
+    else if (Discriminant > 0)
+    {
+        roots.push_back((-B + sqrt(Discriminant)) / (2 * A));
+        roots.push_back((-B - sqrt(Discriminant)) / (2 * A));
+        return roots;
+    }
+    else if (Discriminant == 0)
+    {
+        roots.push_back(-B / (2 * A));
+        return roots;
+    }
+    else
+    {
+        return roots;
+    }
+}
+
+bool ToOutputResult(double &Discriminant, double &A, vector<double> roots)
+{
+    if (A == 0)
+    {
+        cout << "Error, it's a not quadratic equation" << endl;
+        return false;
+    }
+    if (Discriminant > 0)
+    {
+        cout << roots[0] << " " << roots[1] << endl;
+        return true;
+    }
+    else if (Discriminant == 0)
+    {
+        cout << roots[0] << endl;
+        return true;
+    }
+    else
+    {
+        cout << "There is no real root" << endl;
+        return false;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
     {
         printf("\nNumber parameters is wrong");
         return -1;
@@ -16,26 +70,8 @@ int main(int argc, char *argv[])
     double B = atof(numb);
     numb = *&argv[3];
     double C = atof(numb);
-    double Discriminant = B * B - 4 * A * C;
-    if (A == 0)
-    {
-        printf("\nError, it's a not quadratic equation");
-        return -1;
-    }
-    if (Discriminant > 0)
-    {
-        double x1 = (-B + sqrt(Discriminant)) / (2 * A);
-        double x2 = (-B - sqrt(Discriminant)) / (2 * A);
-        printf("Roots of equation: %6.4f %6.4f\n", x1, x2);
-    }
-    else if (Discriminant == 0) 
-    {
-        double x = -B / (2 * A);
-        printf("Root of equation: %6.4f\n", x);
-    }
-    else 
-    {
-        printf("\nThere is no real root");
-    }
+    double discriminant = CalculatingDiscriminant(A, B, C);
+    vector<double> roots = CalculatingRootsOfEquation(A, B, C, discriminant);
+    ToOutputResult(discriminant, A, roots);
     return 0;
 }
