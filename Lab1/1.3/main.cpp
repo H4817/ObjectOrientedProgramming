a42692ba547ce81170d9c5961000cd03334dcc54
nikolai996 <jakimychev.nikolaj@gmail.com>
2016-02-27 16:30:46 +0300

    Update main.cpp

--- Lab1/1.3/main.cpp
+++ Lab1/1.3/main.cpp
@@ -1,80 +1,41 @@
 #include <iostream>
-#include <string.h>
-#include <vector>
+#include <fstream>
 
 using namespace std;
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    bool isPartOfString = false;
-    char line[255];
-    int counter = 0;
-    int counterLines = 1;
-    vector<int> lineNumbers;
-    string inputString;
-
-    if (argc != 3) 
+    if (argc != 3)
     {
-        printf("\nNumber parameters is wrong");
-        return -1;
-    }
-
-    FILE *Fin;
-    Fin = fopen(argv[1], "r");
-    if (Fin == NULL)
-    {
-        printf("\nCan not open file %s", argv[1]);
-        return -1;
-    }
-    inputString = argv[2];
-
-    while (fgets(line, 255, Fin) != NULL) 
-    {
-        string str(line);
-        for (int i = 0; i < str.size(); ++i) 
-        {
-            if (str[i] == inputString[counter])
-            {
-                ++counter;
-                if (counter == inputString.size())
-                {
-                    counter = 0;
-                    if (isPartOfString) 
-                    {
-                        lineNumbers.push_back(counterLines - 1);
-                        isPartOfString = false;
-                    }
-                    lineNumbers.push_back(counterLines);
-                }
-            }
-            else
-            {
-                counter = 0;
-                isPartOfString = false;
-            }
-        }
-        if (counter > 0)
-        {
-            isPartOfString = true;
-        }
-        ++counterLines;
-    }
-    fclose(Fin);
-    if (lineNumbers.size() != 0) 
-    {
-        if(lineNumbers[lineNumbers.size() - 1] == counterLines - 1) 
-        {
-            lineNumbers[lineNumbers.size() - 1] += -1;
-        }
-        for (int i = 0; i < lineNumbers.size(); ++i)
-        {
-            printf("%d\n", lineNumbers[i]);
-        }
-        return 0;
-    }
-    else 
-    {
-        printf("Text not found");
+        cout <<
+        "Number parameters is wrong. The first param which you should put into command line is a filename and the second is string which you want to find" <<
+        endl;
         return 1;
     }
+    ifstream inputFile(argv[1]);
+    if (inputFile == NULL)
+    {
+        cout << "Can't open file " << argv[1] << endl;
+        return 1;
+    }
+    int lineCounter = 1;
+    string inputString = argv[2];
+    string str;
+    bool isStringFound = false;
+    while (getline(inputFile, *&str) != NULL)
+    {
+        if (str.find(inputString) != string::npos)
+        {
+            cout << lineCounter << endl;
+            isStringFound = true;
+        }
+
+        ++lineCounter;
+    }
+    if (!isStringFound)
+    {
+        cout << "Text not found" << endl;
+        return 1;
+    }
+    return 0;
 }
