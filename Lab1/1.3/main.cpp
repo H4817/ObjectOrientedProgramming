2af239d052d7050863994b7e3717e58ae2bc7d81
nikolai996 <jakimychev.nikolaj@gmail.com>
2016-02-27 19:22:59 +0300

    Update main.cpp

--- Lab1/1.3/main.cpp
+++ Lab1/1.3/main.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+vector<int> FindText(ifstream &inputFile, string &inputString)
+{
+    int lineCounter = 1;
+    string str;
+    vector<int> numbersOfStrings;
+    while (getline(inputFile, *&str) != NULL)
+    {
+        if (str.find(inputString) != string::npos)
+        {
+            numbersOfStrings.push_back(lineCounter);
+        }
+        ++lineCounter;
+    }
+    return numbersOfStrings;
+}
+
+void OutputResult(vector<int> numbersOfStrings)
+{
+    for (int i = 0; i < numbersOfStrings.size(); ++i)
+    {
+        cout << numbersOfStrings[i] << endl;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
         cout <<
-        "Number parameters is wrong. The first parameter which you should put into command line is a filename and the second is string which you want to find" <<
+        "Number parameters is wrong. The first param which you should put into command line is a filename and the second is string which you want to find" <<
         endl;
         return 1;
     }
@@ -18,23 +43,16 @@ int main(int argc, char *argv[])
         cout << "Can't open file " << argv[1] << endl;
         return 1;
     }
-    int lineCounter = 1;
     string inputString = argv[2];
-    string str;
-    bool isStringFound = false;
-    while (getline(inputFile, *&str) != NULL)
-    {
-        if (str.find(inputString) != string::npos)
-        {
-            cout << lineCounter << endl;
-            isStringFound = true;
-        }
-        ++lineCounter;
-    }
-    if (!isStringFound)
+    vector<int> result = FindText(inputFile, inputString);
+    if (result.size() == 0)
     {
         cout << "Text not found" << endl;
         return 1;
+    }
+    else
+    {
+        OutputResult(result);
     }
     return 0;
 }
