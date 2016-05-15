adcf33cb0a60b00d6b9db7a60651049059018134
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2016-05-15 21:56:24 +0300

    Update HttpUrl.h

--- Lab6/1.7/HttpUrl.h
+++ Lab6/1.7/HttpUrl.h
@@ -11,40 +11,22 @@ enum Protocol
 class CHttpUrl
 {
 public:
-    // выполняет парсинг строкового представления URL-а, в случае ошибки парсинга
-    // выбрасыват исключение CUrlParsingError, содержащее текстовое описание ошибки
     CHttpUrl(std::string const &url);
 
-    /* инициализирует URL на основе переданных параметров.
-        При недопустимости входных параметров выбрасывает исключение
-        std::invalid_argument
-        Если имя документа не начинается с символа /, то добавляет / к имени документа
-    */
-    CHttpUrl(
+     CHttpUrl(
             std::string const &domain,
             std::string const &document,
             Protocol protocol = HTTP,
             unsigned short port = 80);
 
-    // возвращает строковое представление URL-а. Порт, являющийся стандартным для
-    // выбранного протокола (80 для http и 443 для https) в URL не должен включаться
     std::string GetURL() const;
 
-    // возвращает доменное имя
     std::string GetDomain() const;
 
-    /*
-        Возвращает имя документа. Примеры:
-            /
-            /index.html
-            /images/photo.jpg
-    */
     std::string GetDocument() const;
 
-    // возвращает тип протокола
     Protocol GetProtocol() const;
 
-    // возвращает номер порта
     unsigned short GetPort() const;
 
     bool IsProtocolInUrl(const std::string & protocol, const std::string & url) const;
