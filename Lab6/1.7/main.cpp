ddc362dbd15003d464a23092cb99186b89cc284f
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2016-05-18 03:20:38 +0300

    Update main.cpp

--- Lab6/1.7/main.cpp
+++ Lab6/1.7/main.cpp
@@ -3,10 +3,39 @@
 
 using namespace std;
 
+
+void PrintInformationAboutUrl(const CHttpUrl &cHttpUrl)
+{
+    cout << "Domain: " << cHttpUrl.GetDomain() << endl;
+    cout << "Port: " << cHttpUrl.GetPort() << endl;
+    cout << "Document: " << cHttpUrl.GetDocument() << endl;
+    cout << "Protocol: " << cHttpUrl.GetProtocol() << endl;
+    cout << "Url: " << cHttpUrl.GetURL() << endl;
+}
+
+void WorkWithUser()
+{
+    cout << "Type some commands or press q for exit" << endl;
+    string str;
+    for (; ;)
+    {
+        cout << "> ";
+        cin >> str;
+        if (str == "q")
+        {
+            break;
+        }
+        else
+        {
+            CHttpUrl cHttpUrl(str);
+            PrintInformationAboutUrl(cHttpUrl);
+        }
+    }
+}
+
 int main()
 {
-    CHttpUrl cHttpUrl("http://www.google.com:72");
-    cout << cHttpUrl.GetDomain() << endl;
-    cout << cHttpUrl.GetPort() << endl;
+    WorkWithUser();
+
     return 0;
 }
