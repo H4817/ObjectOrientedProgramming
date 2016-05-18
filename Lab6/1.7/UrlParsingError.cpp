aced39ec27dc8ffa2aef4a12fdec14bc2abb7d04
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2016-05-18 03:21:38 +0300

    Update UrlParsingError.cpp

--- Lab6/1.7/UrlParsingError.cpp
+++ Lab6/1.7/UrlParsingError.cpp
@@ -1,17 +1,6 @@
 #include "UrlParsingError.h"
 
-CUrlParsingError::CUrlParsingError(std::string invalidArgument) try : std::invalid_argument(invalidArgument),
-                                                                      m_invalidArgument(invalidArgument)
+CUrlParsingError::CUrlParsingError(std::string invalidArgument) : std::invalid_argument(invalidArgument)
 {
 
-}
-
-catch (std::exception const& e)
-{
-    std::cout << "Error: " << e.what() << std::endl;
-}
-
-catch (...)
-{
-    std::cout << "Unable to construct CUrlParsingError instance, unknown exception has been caught\n";
 }
