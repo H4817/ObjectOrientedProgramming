dc76c80f97e38dc1a6217322c4b97e1e9a8f3176
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2016-05-22 22:27:53 +0300

    ArraySum

---
+++ Lab7/1.4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+T ArraySum(std::vector<T> const &arr)
+{
+    T arraySum = T();
+    for (auto it : arr)
+    {
+        arraySum += it;
+    }
+    return arraySum;
+}
+
+int main()
+{
+    vector<string> testVec;
+    testVec.push_back("Hello");
+    testVec.push_back(" friend");
+    testVec.push_back(".");
+    cout << ArraySum(testVec) << endl;
+    return 0;
+}
