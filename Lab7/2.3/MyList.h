7f448841405b59181faa91aac307b24ead9854a2
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2016-05-28 15:00:10 +0300

    add Push implementation

--- Lab7/2.3/MyList.h
+++ Lab7/2.3/MyList.h
@@ -25,25 +25,42 @@ public:
 
     void Clear();
 
+    void Push(const T & value = T())
+    {
+        try
+        {
+            node* newNode = new node(value, nullptr, m_tail);
+            if(m_head == nullptr)
+                m_head = newNode;
+            if(m_tail != nullptr)
+                m_tail->m_next = newNode;
+            m_tail = newNode;
+            ++m_elements;
+        }
+        catch (std::exception const &e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+    }
 private:
     struct node
     {
-        int data;
-        node *next, *prev;
+        T m_data;
+        node *m_next, *m_prev;
 
         node(T const &data, node *next, node *prev)
-                : data(data), next(next), prev(prev)
+                : m_data(data), m_next(next), m_prev(prev)
         {
         }
 
         node(T &&data, node *next, node *prev)
-                : data(std::move(data)), next(next), prev(prev)
+                : m_data(std::move(data)), m_next(next), m_prev(prev)
         {
         }
     };
 
-    node *head = nullptr;
-    node *tail = nullptr;
+    node *m_head = nullptr;
+    node *m_tail = nullptr;
     size_t m_elements = 0;
 };
 
--- Lab7/2.3/tests.cpp
+++ Lab7/2.3/tests.cpp
@@ -3,6 +3,25 @@
 #include <boost/test/included/unit_test.hpp>
 #include "MyList.h"
 
+BOOST_AUTO_TEST_SUITE(initialization_tests)
+    BOOST_AUTO_TEST_CASE(should_being_initialized_by_default_value_type)
+    {
+        BOOST_CHECK_NO_THROW(CMyList <int> cMyList1);
+    }
+
+    BOOST_AUTO_TEST_CASE(should_push_numbers_and_strings)
+    {
+        CMyList <int> cMyList1;
+        BOOST_CHECK_NO_THROW(cMyList1.Push(3));
+        BOOST_CHECK_NO_THROW(cMyList1.Push(1.f));
+        BOOST_CHECK_NO_THROW(cMyList1.Push());
+        CMyList <std::string> cMyList2;
+        BOOST_CHECK_NO_THROW(cMyList2.Push("123"));
+        BOOST_CHECK_NO_THROW(cMyList2.Push("test"));
+        BOOST_CHECK_NO_THROW(cMyList2.Push());
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
 
 
 
